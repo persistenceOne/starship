4939e4bd1f9a3e472d53b6c956d7458e1727337f
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2023-07-06 17:47:45 +0530

    update genesis validator's commission rates

--- charts/devnet/scripts/setup_genesis.sh
+++ charts/devnet/scripts/setup_genesis.sh
@@ -21,7 +21,12 @@ jq -r ".genesis[0].mnemonic" $KEYS_CONFIG | $CHAIN_BIN keys add $(jq -r ".genesi
 $CHAIN_BIN $CHAIN_GENESIS_CMD add-genesis-account $($CHAIN_BIN keys show -a $(jq -r .genesis[0].name $KEYS_CONFIG) --keyring-backend="test") $COINS --keyring-backend="test"
 
 echo "Creating gentx..."
-$CHAIN_BIN $CHAIN_GENESIS_CMD gentx $(jq -r ".genesis[0].name" $KEYS_CONFIG) 5000000000$DENOM --keyring-backend="test" --chain-id $CHAIN_ID
+$CHAIN_BIN $CHAIN_GENESIS_CMD gentx $(jq -r ".genesis[0].name" $KEYS_CONFIG) 5000000000$DENOM \
+  --keyring-backend="test" \
+  --chain-id $CHAIN_ID \
+  --commission-rate 0.02 \
+  --commission-max-rate 0.05 \
+  --commission-max-change-rate 0.01
 
 echo "Output of gentx"
 cat $CHAIN_DIR/config/gentx/*.json | jq
